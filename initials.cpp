681272eb10ee9f99865aa177c958b3ad50485f22
Md Tabassum Hossain Emon <60403153+Emon-ProCoder7@users.noreply.github.com>
2021-05-09 17:09:40 +0600

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// I give much attention to detail.
+
+
+int main(){
+
+  std::cout << "EEEEEE   M        M\n";
+  std::cout << "E        M M    M M\n";
+  std::cout << "E        M   MM   M\n";
+  std::cout << "EEEE     M        M\n";
+  std::cout << "E        M        M\n";
+  std::cout << "E        M        M\n";
+  std::cout << "EEEEEE   M        M\n";
+}
